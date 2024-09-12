6be3f04ad59447dbfb90ea12e9a472d983838150
WefNET <john.wefler@gmail.com>
2024-09-12 07:00:40 -0500

    Added author and date to posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,8 @@ const blogCollection = defineCollection({
     title: z.string(),
     tags: z.array(z.string()),
     image: z.string().optional(),
+    author: z.string(),
+    published: z.date(),
     }),
 });
 
