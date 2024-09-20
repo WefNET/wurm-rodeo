58fe469bf150a07e715f32f15702ae50fc18e80f
WefNET <john.wefler@gmail.com>
2024-09-20 18:16:06 -0500

    Setting trailing slash never

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
+  trailingSlash: 'never',
   vite: {
     server: {
       watch: { usePolling: true },
