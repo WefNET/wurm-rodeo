07cc5fc0afa9ec428d957166b0627a0a7defbfff
WefNET <john.wefler@gmail.com>
2024-10-19 09:13:17 -0500

    Updating SiteMap, Robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  site: 'https://wurm.rodeo',
+  integrations: [
+    mdx(),
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.8,
+    })
+  ],
   trailingSlash: 'never',
   vite: {
     server: {
