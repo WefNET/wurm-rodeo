ee44f5bc1f249abb887ef8eb4ae4ad5ecdf9b655
WefNET <john.wefler@gmail.com>
2024-12-23 15:11:17 -0600

    Tracking updates

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,11 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
 });
+
+import Clarity from "@microsoft/clarity";
+
+const projectId = "pinack8wwl";
+
+Clarity.init(projectId);
+Clarity.consent();
+
